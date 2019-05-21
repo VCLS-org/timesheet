0056de811189d6441e2033c56022783b4a842241
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-05-21 16:48:45 +0200

    Added timesheet_by_seniority

---
+++ sale_project_timesheet_by_seniority/models/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from . import account_analytic_line
+from . import hr_employee
+from . import hr_employee_seniority_level
+from . import product_template
+from . import sale
